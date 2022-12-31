a0ff084833ec7c30a8fd4b436ceb72a4c3fd6aec
UsmanB-H <121515322+UsmanB-H@users.noreply.github.com>
2022-12-31 14:17:09 -0500

    Test Commit

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        onPress={() => {}} 
+        onPress={() => {}}
         style={{
-          backgroundColor:'white',
+          backgroundColor: "white",
           padding: 15,
           borderRadius: 10,
-          marginBottom: 30
-        }}>
-        <Text style={{
-          textAlign:'center',
-          fontWeight: '800',
-          fontSize: 15,
-          color: "#5A5A5A"
-        }}>Welcome to Presidential Stocks</Text>
+          marginBottom: 30,
+        }}
+      >
+        <Text
+          style={{
+            textAlign: "center",
+            fontWeight: "800",
+            fontSize: 15,
+            color: "#5A5A5A",
+          }}
+        >
+          Welcome to Presidential Stock
+        </Text>
       </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
@@ -28,8 +33,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1652f0',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1652f0",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
