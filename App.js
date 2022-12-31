8bd2e519c864813fd6e75303ad8f7938482b0e7b
Usman-BH <usmanbenrbia@gmail.com>
2022-12-31 13:57:39 -0500

    inital commit

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working (test commit)!</Text>
+      <TouchableOpacity
+        onPress={() => {}} 
+        style={{
+          backgroundColor:'white',
+          padding: 15,
+          borderRadius: 10,
+          marginBottom: 30
+        }}>
+        <Text style={{
+          textAlign:'center',
+          fontWeight: '800',
+          fontSize: 15,
+          color: "#5A5A5A"
+        }}>Welcome to Presidential Stocks</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1652f0',
     alignItems: 'center',
     justifyContent: 'center',
   },
