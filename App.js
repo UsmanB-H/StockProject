7f8cd43acf09bcf14e2b71556b77a57b2f9424db
UsmanB-H <121515322+UsmanB-H@users.noreply.github.com>
2022-12-31 16:39:27 -0500

    Added Inital Page

--- App.js
+++ App.js
@@ -1,32 +1,46 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  SafeAreaView,
+  Image,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => {}}
-        style={{
-          backgroundColor: "white",
-          padding: 15,
-          borderRadius: 10,
-          marginBottom: 30,
-        }}
-      >
-        <Text
-          style={{
-            textAlign: "center",
-            fontWeight: "800",
-            fontSize: 15,
-            color: "#5A5A5A",
+    <>
+      <SafeAreaView style={styles.safeViewContainer}></SafeAreaView>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: "https://cdn.freebiesupply.com/logos/large/2x/stock-logo-png-transparent.png",
           }}
-        >
-          Welcome to Presidential Stocks
-        </Text>
-      </TouchableOpacity>
-      <StatusBar style="auto" />
-    </View>
+          style={{ width: 300, height: 700 }}
+          resizeMode="contain"
+        />
+        <View style={styles.ButtonContainer}>
+          <View style={styles.LoginContainer}>
+            <Button
+              style={styles.LoginButton}
+              title="Login In"
+              color="#5A5A5A"
+            ></Button>
+          </View>
+          <View style={styles.SignUpContainer}>
+            <Button
+              style={styles.SignUpButton}
+              title="Sign Up"
+              color="#5A5A5A"
+            ></Button>
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
@@ -37,4 +51,23 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  ButtonContainer: {
+    justifyContent: "space-evenly",
+    flexDirection: "row",
+  },
+  LoginContainer: {
+    backgroundColor: "#FFFF",
+    padding: 2,
+    borderRadius: 10,
+    marginRight: 20,
+  },
+  SignUpContainer: {
+    backgroundColor: "#FFFF",
+    padding: 2,
+    borderRadius: 10,
+  },
+  safeViewContainer: {
+    backgroundColor: "#1652f0",
+  },
+  ImageContainer: {},
 });
